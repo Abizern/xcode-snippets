bc3d9c70360c916cb4516af5a2462c4c5484e8ff
Abizer Nasir <abizern@abizern.org>
2015-10-09 21:05:52 +0100

    Add singleton snippet

---
+++ singleton.swift
@@ -0,0 +1,15 @@
+---
+title: "Shared Singleton"
+summary: "Definition for a singleton class"
+completion-scope: Class Implementation
+---
+
+class <#Singleton#> {
+
+    static let sharedInstance = <#Singleton#>()
+
+    init() {
+        <#code#>
+    }
+    
+}
