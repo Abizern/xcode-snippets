7461f09ec9f4fb00e0bd37b57dd635c22beddf0e
Abizer Nasir <abizern@abizern.org>
2015-10-09 21:05:14 +0100

    Add imv snippet

---
+++ imv.swift
@@ -0,0 +1,8 @@
+---
+title: "ImageView"
+summary: "Create & Initialize UIImageView with Named Image"
+platform: iOS
+completion-scope: Code Expression
+---
+
+let <#imageView#> = UIImageView(image: UIImage(named: <#T##String#>))
