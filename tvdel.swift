5fd8f749c721199299afe04f5478ed551daed67f
Abizer Nasir <abizern@abizern.org>
2015-10-09 21:06:13 +0100

    Add tvdel snippet

---
+++ tvdel.swift
@@ -0,0 +1,14 @@
+---
+title: "UITableViewDelegate"
+summary: "Placeholders for required UITableViewDelegate protocol methods"
+platform: iOS
+completion-scope: Class Implementation
+---
+
+extension <#Class#> : UITableViewDelegate {
+
+    func tableView(tableView: UITableView, didDeselectRowAtIndexPath indexPath: NSIndexPath) {
+        <#code#>
+    }
+    
+}
