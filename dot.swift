2634ba3c50c4cd904e10e8a4faf4dd25316804ff
Abizer Nasir <abizern@abizern.org>
2015-10-10 11:21:28 +0100

    Add dot snippet

---
+++ dot.swift
@@ -0,0 +1,11 @@
+---
+title: "Basic do-catch block"
+summary: "A basic do-catch block with templates for the code and error handlers"
+completion-scope: Function or Method
+---
+
+do {
+    <#code#>
+} catch {
+    <#code#>
+}
